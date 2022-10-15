6f8491fb5b09194c7d89f8b4aa07a55f11f56f65
Trung Kien <kiennthe153296@fpt.edu.vn>
2022-10-15 12:26:12 +0700

    spawn enemies va enemy duoi theo player

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private float speed = 0.4f;
+    private GameObject player;
+    private Vector2 direction;
+    internal bool foundPlayer = false;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 turretPosition = GameObject.Find("Tower").transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, turretPosition, Time.deltaTime * speed);
+        if (Vector2.Distance(transform.position, player.transform.position) < 3)
+        {
+            foundPlayer = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (foundPlayer)
+        {
+            direction = (player.transform.position - transform.position).normalized;
+        }
+        transform.Translate(direction * speed * Time.smoothDeltaTime);
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Player"))
+    //    {
+    //        transform.position = Vector2.MoveTowards(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position, Time.deltaTime * speed);
+    //    }
+    //}
+}
