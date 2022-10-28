f03ee348846ba419188acb3d2ce8916b5a274fea
SethKangg <maybead10x@gmail.com>
2022-10-28 23:22:40 +0700

    Base AbsFactory

---
+++ Assets/AbsFactory/FireFactory.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFactory : EnemyFactory
+    
+{
+    private const string FireRangeName = "Fire_range";
+    private const string FireMeleeName = "Fire_melee";
+
+    public override void createMelee()
+    {
+        var FireRangeObj = Resources.Load(FireRangeName) as GameObject;
+        if(FireRangeObj != null)
+        {
+                Instantiate(FireRangeObj.transform, new Vector2(1, 1),Quaternion.identity);
+        }
+        else
+        {
+            throw new System.ArgumentException(FireRangeObj + " not found");
+        }
+    }
+
+    public override void createRange()
+    {
+        var FireMeleeObj = Resources.Load(FireMeleeName) as GameObject;
+        if (FireMeleeObj != null)
+        {
+            Instantiate(FireMeleeObj.transform, new Vector2(1, 0), Quaternion.identity);
+        }
+        else
+        {
+            throw new System.ArgumentException(FireMeleeObj + " not found");
+        }
+    }
+
+   
+}
